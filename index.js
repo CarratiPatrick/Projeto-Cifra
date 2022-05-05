dd6faca3bdf6196831b100a5e079a89e22c41c65
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-05-05 18:55:49 -0300

    terminei o projeto

--- index.js
+++ index.js
@@ -16,3 +16,77 @@ escolha.addEventListener("change", function () {
     cesar.style.display = "none";
   }
 });
+
+// escolher codificar ou decodificar 
+var checkCesar = document.querySelectorAll(".check")
+var checkBase = document.querySelectorAll(".check2")
+var botaoCifra = document.querySelector("#cifra")
+var botaoCifra2 = document.querySelector("#cifra2")
+
+//cifra 64
+var entrada64 = document.querySelector("#entrada2")
+var saida2 = document.querySelector("#saida2")
+var valorBase
+entrada64.addEventListener("keyup", function () {
+  valorBase = entrada64.value;
+  if(checkBase[0].checked){
+    saida2.value = btoa(valorBase);
+  }else if (checkBase[1].checked){
+    saida2.value = atob(valorBase)
+  }
+})
+
+
+// cifra cesar
+var entrada = document.querySelector("#entrada")
+var saida = document.querySelector("#saida")
+var chaveInc = document.querySelector("#chaveInc")
+var valorChave;
+entrada.addEventListener("keyup", function(){
+  if(checkCesar[0].checked){
+    var valorTexto = entrada.value.split("")
+    valorChave = parseInt(chaveInc.value)
+    saida.value = codifica(valorTexto, valorChave)
+  }else{
+    var valorTexto = entrada.value.split("")
+    valorChave = parseInt(chaveInc.value)
+    saida.value = decodifica(valorTexto, valorChave)
+  }
+})
+function codifica(txt, chaveInc){
+  return(
+    txt
+    .map((str)=>{
+      var cripto = str.charCodeAt();
+      if (cripto >= 65 && cripto<=90){
+        return String.fromCharCode(((cripto - 65 + chaveInc)%26)+65);
+      }else if(cripto >= 97 && cripto <=122){
+        return String.fromCharCode(((cripto-97+chaveInc)%26)+97);
+      }else{
+        return str;
+      }
+    })
+    .join("")
+  )
+  
+  }
+  function decodifica(txt , chaveInc) {
+    return (
+      txt
+      .map((str)=>{
+        var cripto = str.charCodeAt();
+        if(cripto >= 65 && cripto<=90){
+         return String.fromCharCode(cripto - 65 - chaveInc < 0)
+         ?
+         String.fromCharCode(((cripto - 65 - chaveInc + 26) % 26) + 65)
+         : String.fromCharCode(((cripto - 65 - chaveInc) % 26) + 65);
+        }else if (cripto >= 97 && cripto <= 122){
+          return String.fromCharCode(cripto - 97 - chaveInc < 0)
+            ? String.fromCharCode(((cripto - 97 - chaveInc + 26) % 26) + 97)
+            : String.fromCharCode(((cripto - 97 - chaveInc) % 26) + 65);
+        }
+      })
+      .join("")
+    )
+    
+}
